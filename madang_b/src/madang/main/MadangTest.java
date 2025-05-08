2ad98f5695daf9aa06290bf665ecda64858548b4
Sena <134036193+SenaNam@users.noreply.github.com>
2025-05-08 15:39:09 +0900

    [add] 하느님이

--- madang_b/src/madang/main/MadangTest.java
+++ madang_b/src/madang/main/MadangTest.java
@@ -13,7 +13,8 @@ public class MadangTest {
 // 동해물과 백두산이 마르고 닳도록
 // 동해물과 백두산이 마르고 닳도록
 // 동해물과 백두산이 마르고 닳도록
-		System.out.println("Hello World1");
+		
+		System.out.println("안녕하세요");
 		System.out.println("Hello World2");
 		System.out.println("Hello World3");
 		System.out.println("Hello World4");
@@ -21,6 +22,12 @@ public class MadangTest {
 		System.out.println("Hello World6");
 		System.out.println("Hello World7");
 		System.out.println("Hello World8");
+		
+		// 하느님이 보우하사 우리 나라만세
+		// 하느님이 보우하사 우리 나라만세
+		// 하느님이 보우하사 우리 나라만세
+		// 하느님이 보우하사 우리 나라만세
+		
 
 	}
 
